0b1480ee8fb3a97555d10f3f58c9828d3f699102
kevinmondragon426 <kevinduban77@gmail.com>
2023-09-05 11:22:47 -0500

    addbd

--- casaberrio/core/admin.py
+++ casaberrio/core/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Inventory)
-admin.site.register(InventoryType)
-admin.site.register(Supplier)
-admin.site.register(Low)
-admin.site.register(Rent)
-admin.site.register(Menaje)
+
+
+admin.site.register(Equipment)
 admin.site.register(Catering)
-admin.site.register(Decor)
+admin.site.register(Drink)
+admin.site.register(Property)
+admin.site.register(ProductType)
+admin.site.register(Inventory)
+admin.site.register(Supplier)
+admin.site.register(Rent)
+admin.site.register(StateProduct)
+admin.site.register(TypePay)
 admin.site.register(Pay)
-admin.site.register(Event)
 admin.site.register(EventType)
-admin.site.register(Menu)
-admin.site.register(Drinks)
 admin.site.register(Campus)
 admin.site.register(Lounge)
+admin.site.register(Event)
 admin.site.register(loyalty)
 admin.site.register(TypePqrsd)
 admin.site.register(StatePqrsd)
--- casaberrio/core/models.py
+++ casaberrio/core/models.py
@@ -1,14 +1,108 @@
-from tkinter import E
 from django.db import models
 
+class Property(models.Model):
+    chairs = models.PositiveIntegerField( verbose_name='Sillas')
+    table = models.PositiveIntegerField(verbose_name='Mesas')
+    chairs_and_tables = models.PositiveIntegerField(verbose_name='Sillas y mesas')
+    tents = models.PositiveIntegerField(verbose_name='Carpas')
+    none = models.PositiveIntegerField(verbose_name='Ninguno')
 
+    def __str__(self):
+        return self.chairs
+
+    class Meta:
+        verbose_name = 'Inmueble'
+        verbose_name_plural = 'Inmuebles'
+        db_table = 'inmueble'
+        ordering = ['id']
+
+class Drink(models.Model):
+    schnapps = models.PositiveIntegerField(verbose_name='Aguardiente')
+    rum = models.PositiveIntegerField(verbose_name='Ron')
+    whiskey = models.PositiveIntegerField(verbose_name='Whisky')
+    cocktails = models.PositiveIntegerField(verbose_name='Cócteles')
+    soda = models.PositiveIntegerField(verbose_name='Gaseosa')
+
+    def __str__(self):
+        return self.schnapps
+
+    class Meta:
+        verbose_name = 'Bebida'
+        verbose_name_plural = 'Bebidas'
+        db_table = 'bebida'
+        ordering = ['id']
+
+class Catering(models.Model):
+    household = models.PositiveIntegerField(verbose_name='Menaje')
+    table_linen = models.PositiveIntegerField(verbose_name='Manteleria')
+    waiters = models.PositiveIntegerField(verbose_name='Meseros')
+    menu = models.TextField(verbose_name='Menu')
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.household
+
+    class Meta:
+        verbose_name = 'Catering'
+        verbose_name_plural = 'Catering'
+        db_table = 'catering'
+        ordering = ['id']
+
+class Equipment(models.Model):
+    lightning = models.PositiveIntegerField(verbose_name='Iluminación')
+    sound = models.PositiveIntegerField(verbose_name='Sonido')
+
+    def __str__(self):
+        return self.lightning
+
+    class Meta:
+        verbose_name = 'Equipo'
+        verbose_name_plural = 'Equipos'
+        db_table = 'equipos'
+        ordering = ['id']
+
+class Rent(models.Model):
+    rental_date_and_time = models.DateTimeField(verbose_name='Fecha y hora de alquiler')
+    return_date_and_time = models.DateTimeField(verbose_name='Fecha y hora de devolución')
+    description = models.CharField(max_length=500, verbose_name='Descripcion')
+    unit_price = models.PositiveIntegerField(verbose_name='Precio Unitario')
+    total_price = models.PositiveIntegerField(verbose_name='Precio total')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    catering = models.ForeignKey(Catering, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.rental_date_and_time
+
+    class Meta:
+        verbose_name = 'Alquiler'
+        verbose_name_plural = 'Alquileres'
+        db_table = 'alquiler'
+        ordering = ['id']
+
+
+class ProductType(models.Model):
+    equipment = models.OneToOneField(Equipment, on_delete=models.CASCADE)
+    property = models.OneToOneField(Property, on_delete=models.CASCADE)
+    drink = models.OneToOneField(Drink, on_delete=models.CASCADE)
+    catering = models.OneToOneField(Catering, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Tipo de Producto'
+
+    class Meta:
+        verbose_name = 'Tipo Producto'
+        verbose_name_plural = 'Tipos de Productos'
+        db_table = 'productotipo'
+        ordering = ['id']
 
 class Inventory (models.Model):
-    availability = models.CharField(max_length=100, verbose_name='Disponibilida')
+    availability = models.CharField(max_length=100, verbose_name='Disponibilidad')
     worth = models.PositiveIntegerField(verbose_name ='Valor')
     description = models.CharField( max_length=500, verbose_name='Descripcion')
     name = models.CharField(max_length=100,verbose_name= 'Nombre')
-    type_inventory =models.CharField(max_length=100,verbose_name= 'Tipo de inventario')
+    productType = models.ForeignKey (ProductType, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.availability
@@ -18,22 +112,6 @@ class Inventory (models.Model):
         verbose_name_plural='Inventarios'
         db_table='inventario'
         ordering=['id']
-
-class InventoryType(models.Model):
-    real_estate =models.CharField(max_length=100,verbose_name= 'Inmobiliaria')
-    equipment =models.CharField(max_length=100,verbose_name= 'Equipos')
-    props =models.CharField(max_length=100,verbose_name= 'Utileria')
-    inventory = models.ForeignKey (Inventory, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.real_estate
-
-    class Meta:
-        verbose_name='Tipo de Inverntario'
-        verbose_name_plural='Tipos de Inventarios'
-        db_table='tipo_inventario'
-        ordering=['id']
-
 
 class Supplier (models.Model):
     nit = models.PositiveIntegerField(verbose_name = 'NIT')
@@ -50,76 +128,38 @@ class Supplier (models.Model):
         ordering=['id']
 
 
-class Low (models.Model):
-    description = models.CharField(max_length=500, verbose_name='Descripcion')
+class StateProduct  (models.Model):
+    asset = models.CharField(max_length=100, verbose_name='Activo ')
+    idle = models.CharField(max_length=100, verbose_name='Inactivo ')
     amount = models.PositiveIntegerField(verbose_name= 'Cantidad')
+    description = models.CharField(max_length=500,  verbose_name='Descripcion')    
     inventory = models.ForeignKey (Inventory, on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return self.description
+        return self.asset
 
     class Meta:
-        verbose_name='Baja'
-        verbose_name_plural = 'Bajas'
-        db_table='bajas'
-        ordering=['id']
+        verbose_name = 'Estado de producto '
+        verbose_name_plural = 'Estados de producto '
+        db_table = 'estadoproducto'
+        ordering = ['id']
 
-class Menaje(models.Model):
-    name_menaje = models.CharField(max_length=20, verbose_name='nombre del menaje')
-    
+class TypePay(models.Model):
+    nequi = models.CharField(max_length=100,)
+    daviplata= models.CharField (max_length=100,)
+    bamcolombia = models.CharField(max_length=100,)
+    efectivo = models.CharField(max_length=100,)
+    tarjeta = models.CharField(max_length=100,)
+
     def __str__(self):
-        return self.name_menaje
+        return self.nequi
 
     class Meta:
-        verbose_name='Menaje'
-        verbose_name_plural = 'Menajes'
-        db_table='Menaje'
-        ordering=['id']
-
-class Catering(models.Model):
-    name_catering = models.CharField(max_length=20, verbose_name='nombre del menu')
-    
-    def __str__(self):
-        return self.name_catering
-
-    class Meta:
-        verbose_name='Menú'
-        verbose_name_plural = 'Menús'
-        db_table='Menú'
-        ordering=['id']
-
-class Decor(models.Model):
-    name_decor = models.CharField(max_length=20, verbose_name='nombre de la decoracion')
-    
-    def __str__(self):
-        return self.name_decor
-
-    class Meta:
-        verbose_name='Decoracion'
-        verbose_name_plural = 'Decoraciones'
-        db_table='Decoracion'
-        ordering=['id']
-
-class Rent(models.Model):
-    Name = models.CharField(max_length=50, verbose_name='Nombre')
-    Phone = models.PositiveIntegerField(verbose_name='Telefono')
-    Email = models.EmailField(verbose_name='Email')
-    Date = models.DateField(verbose_name='Fecha')
-    Hour = models.TimeField(verbose_name='Hora')
-    Catering = models.ManyToManyField('Catering')
-    Menaje = models.ManyToManyField('Menaje')
-    Decor = models.ManyToManyField('Decor')
-    
-    def __str__(self):
-        return self.Name
-
-    class Meta:
-        verbose_name='Alquiler'
-        verbose_name_plural='Alquileres'
-        db_table='Alquiler'
-        ordering=['id']
-        
+        verbose_name = 'Tipo de pago  '
+        verbose_name_plural = 'Tipos de pago '
+        db_table = 'tipopago'
+        ordering = ['id']
 
 class Pay(models.Model) :
     subscription_number = models.PositiveIntegerField(verbose_name='Numero de abono')
@@ -137,27 +177,7 @@ class Pay(models.Model) :
         verbose_name_plural='Pagos'
         db_table='pago'
         ordering=['id']
-
-class Event(models.Model):
-    date = models.DateField( verbose_name='Fecha')
-    hour = models.TimeField(verbose_name='Hora')
-    event_type = models.CharField(max_length=100, verbose_name='Tipo de evento')
-    theme = models.CharField(max_length=200, verbose_name='Tematica')
-    description = models.CharField(max_length=500,  verbose_name='Descripcion')
-    number_of_invites = models.PositiveIntegerField(verbose_name='Numero de invitados')
-    special_need = models.CharField(max_length=200, verbose_name='Nesecidad especial')
-    guest_document = models.CharField(max_length=200, verbose_name='Documento invitado')
-    pay = models.ForeignKey (Pay, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.date
-
-    class Meta:
-        verbose_name='Evento'
-        verbose_name_plural='Eventos'
-        db_table='evento'
-        ordering=['id']
-
+ 
 class EventType (models.Model):
     wedding = models.CharField(max_length=100, verbose_name='Boda')
     birthday = models.CharField(max_length=100, verbose_name='Cumpleaños')
@@ -165,54 +185,20 @@ class EventType (models.Model):
     baby_shower = models.CharField(max_length=100, verbose_name='Baby shower')
     conferences = models.CharField(max_length=100, verbose_name='Conferencias')
     theme_parties = models.CharField(max_length=100, verbose_name='Fisestas tematicas')
-    event = models.ForeignKey (Event, on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return self.wedding
 
     class Meta:
         verbose_name='Tipo evento'
         verbose_name_plural='Tipos de evento'
-        db_table='tipo de evento'
+        db_table='tipoevento'
         ordering=['id']
-
-
-class Menu(models.Model):
-    name_menu = models.CharField(max_length=100, verbose_name='Nombre menu')
-    description = models.CharField(max_length= 500,  verbose_name='Descripcion')
-    price = models.PositiveIntegerField(verbose_name='Precio')
-    event = models.ForeignKey (Event, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name_menu
-
-    class Meta:
-        verbose_name='Menu'
-        verbose_name_plural='Menus'
-        db_table='menu'
-        ordering=['id']
-
-
-class Drinks(models.Model):
-    drink_name = models.CharField(max_length=100, verbose_name='Nombre bebida') 
-    type_drink  = models.CharField(max_length=100, verbose_name='Tipo bebida')
-    worth =models.PositiveIntegerField(verbose_name ='Valor')
-    menu = models.ForeignKey (Menu, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.drink_name
-
-    class Meta:
-        verbose_name='Bebida'
-        verbose_name_plural='Bebidas'
-        db_table='bebida'
-        ordering=['id']
-
-
 
 class Campus (models.Model):
     headquarters_name = models.CharField(max_length=100,verbose_name='Nombre sede')
     address = models.CharField(max_length=100,verbose_name='Dirección')
-    event = models.ForeignKey (Event, on_delete=models.CASCADE)
+    
     
 
     def __str__(self):
@@ -223,7 +209,6 @@ class Campus (models.Model):
         verbose_name_plural='Sedes'
         db_table='sede'
         ordering=['id']
-
 
 class Lounge (models.Model):
     availability  = models.CharField(max_length=100, verbose_name='Disponibilidad')
@@ -239,6 +224,28 @@ class Lounge (models.Model):
         db_table='salon'
         ordering=['id']
 
+class Event(models.Model):
+    date = models.DateField( verbose_name='Fecha')
+    hour = models.TimeField(verbose_name='Hora')
+    event_type = models.CharField(max_length=100, verbose_name='Tipo de evento')
+    theme = models.CharField(max_length=200, verbose_name='Tematica')
+    description = models.CharField(max_length=500,  verbose_name='Descripcion')
+    number_of_invites = models.PositiveIntegerField(verbose_name='Numero de invitados')
+    special_need = models.CharField(max_length=200, verbose_name='Nesecidad especial')
+    guest_document = models.CharField(max_length=200, verbose_name='Documento invitado')
+    pay = models.ForeignKey (Pay, on_delete=models.CASCADE)
+    eventType = models.ForeignKey (EventType, on_delete=models.CASCADE)
+    campus = models.ForeignKey (Campus, on_delete=models.CASCADE)
+    lounge = models.ForeignKey (Lounge, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.date
+
+    class Meta:
+        verbose_name='Evento'
+        verbose_name_plural='Eventos'
+        db_table='evento'
+        ordering=['id']
 
 class loyalty (models.Model):
     reward  =models.CharField(max_length=100,verbose_name= 'Recompensa')
@@ -290,6 +297,3 @@ class StatePqrsd (models.Model):
         verbose_name_plural=' Estados pqrsd '
         db_table='estado pqrsd'
         ordering=['id']
-
-
-
