ab3200221d3386986ce52ded7574da764caee6c7
Eliana349 <elianaosorio349@gmail.com>
2023-11-16 14:16:22 -0500

    delelte
    dasfas

--- casaberrio/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Register
-
-class formularioRegistro(forms.ModelForm):
-    class Meta:
-        model = Register  
-        fields = ['name', 'lastname', 'username', 'phone', 'email', 'password', 'confirm_password']
